d6176b35397327bbfcc4e5789a9748b253adb954
perennialcode1 <Chaitanya@perennialcode.in>
2023-10-17 12:17:29 +0530

    updated3_commit

--- otProject/settings.py
+++ otProject/settings.py
@@ -34,9 +34,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ijcljlv9*o0*k*d6=^#5k)v@*d^47w%*_7kqt1z%gnh33hfa+b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
